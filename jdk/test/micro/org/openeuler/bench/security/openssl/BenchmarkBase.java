c63bde7cca16f048349f0b6273286fd0e051252b
Noah <hedongbo@huawei.com>
2021-04-22 21:10:57 +0800

    I3NPVZ: change Mode.AverageTime to Mode.Throughput in BenchmarkBase.java

--- jdk/test/micro/org/openeuler/bench/security/openssl/BenchmarkBase.java
+++ jdk/test/micro/org/openeuler/bench/security/openssl/BenchmarkBase.java
@@ -45,8 +45,8 @@ import java.security.Security;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
 @Warmup(iterations = 2, time = 2, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
 @Fork(jvmArgsPrepend = {"-Xms100G", "-Xmx100G", "-XX:+AlwaysPreTouch"}, value = 1)
